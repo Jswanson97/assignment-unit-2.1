3a9a2926cbc3883dfb581b4ac5d5ae2f2971bd32
Jesse Swanson <jswanson97@gmail.com>
2023-11-01 13:59:16 -0500

    added full name

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,8 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Jesse';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Swanson';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
